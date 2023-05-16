db8bb90dbfa6b520d53a514a09f423b58b096c19
yigitkarakurt <yigitkarakurt35@gmail.com>
2023-05-16 16:23:41 +0300

    card form pickers

--- SpendingTracker/Main/AddCard/AddCardForm.swift
+++ SpendingTracker/Main/AddCard/AddCardForm.swift
@@ -8,12 +8,56 @@
 import SwiftUI
 
 struct AddCardForm: View {
-    @Environment(\.presentationMode) var presentationMode
     
-    var body: some View{
+    @Environment(\.presentationMode) var presentationMode
+    @State private var name = ""
+    @State private var cardNumber = ""
+    @State private var limit = ""
+    
+    @State private var cardType = "Visa"
+    
+    @State private var month = 1
+    @State private var year = Calendar.current.component(.year, from: Date())
+    @State private var color = Color.blue
+    
+    let currentYear = Calendar.current.component(.year, from: Date())
+    
+    var body: some View {
         NavigationView{
             Form{
-                Text("Add Card Form")
+                Section(header: Text("CARD INFORMATION")){
+                    
+                    TextField("Name", text: $name)
+                    TextField("Credit Card Number", text: $cardNumber)
+                        .keyboardType(.numberPad)
+                    TextField("Credit Limit", text: $limit)
+                        .keyboardType(.numberPad)
+                    
+                    Picker("Type", selection: $cardType) {
+                        ForEach(["Visa", "Mastercard", "Discover"], id: \.self) { cardType in
+                            Text(String(cardType)).tag(String(cardType))
+                        }
+                        
+                    }
+                    
+                }
+                
+                Section(header: Text("EXPIRATION")){
+                    Picker("Month", selection: $month) {
+                        ForEach(1..<13, id: \.self) { num in
+                            Text(String(num)).tag(String(num))
+                        }
+                    }
+                    Picker("Year", selection: $year) {
+                        ForEach(currentYear..<currentYear+25, id: \.self) { num in
+                            Text(String(num)).tag(String(num))
+                        }
+                    }
+                }
+                
+                Section(header: Text("CARD COLOR")){
+                    ColorPicker("Color", selection: $color)
+                }
             }
                 .navigationTitle("Add Credit Card")
                 .navigationBarItems(leading: Button(action: {
