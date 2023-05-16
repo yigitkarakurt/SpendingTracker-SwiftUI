c818b1fb568b04f4cf68af791d92ea2fa355146d
yigitkarakurt <yigitkarakurt35@gmail.com>
2023-05-16 14:36:11 +0300

    show cards in tabView

--- SpendingTracker/Main/MainView.swift
+++ SpendingTracker/Main/MainView.swift
@@ -12,43 +12,64 @@ struct MainView: View {
         NavigationView {
             ScrollView {
                 
-                
-                VStack (alignment: .leading, spacing: 16){
-                    Text("Apple Blue Visa Card")
-                        .font(.system(size: 24, weight: .semibold))
-                    HStack{
-                        Image("visa")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(height: 44)
-                            .clipped()
-                        Spacer()
-                        Text("Balance: 50.000$")
-                            .font(.system(size: 18, weight: .semibold))
+                TabView{
+                    ForEach(0..<5){ num in
+                        CreditCardView()
                     }
-                    
-                    
-                    
-                    Text("1234 1234 1234 1234")
-                    
-                    Text("Credit Limit: 50.000$")
-                    
-                    HStack{ Spacer() }
+                
+                    .padding(.bottom, 50)
                     
                 }
-                .foregroundColor(.white)
-                .padding()
-                .background(Color.blue)
-                .cornerRadius(8)
-                .shadow(radius: 5)
-                .padding(.horizontal)
-                .padding(.top, 8)
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+                .frame(height: 280)
+                .indexViewStyle(.page(backgroundDisplayMode: .always))
+                
             }
             .navigationTitle("Credit Cards")
             .navigationBarItems(trailing: addCardButton)
         }
-        
-        
+    }
+    
+    struct CreditCardView: View{
+        var body: some View{
+            VStack (alignment: .leading, spacing: 16){
+                Text("Apple Blue Visa Card")
+                    .font(.system(size: 24, weight: .semibold))
+                HStack{
+                    Image("visa")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 44)
+                        .clipped()
+                    Spacer()
+                    Text("Balance: 50.000$")
+                        .font(.system(size: 18, weight: .semibold))
+                }
+                
+                
+                
+                Text("1234 1234 1234 1234")
+                
+                Text("Credit Limit: 50.000$")
+                
+                HStack{ Spacer() }
+                
+            }
+            .foregroundColor(.white)
+            .padding()
+            .background(
+                LinearGradient(colors: [Color.blue.opacity(0.4),
+                    Color.blue],
+                    startPoint: .top,
+                    endPoint: .bottom)
+            )
+            .overlay(RoundedRectangle(cornerRadius: 8)
+                .stroke(Color.black.opacity(0.5), lineWidth: 1))
+            .cornerRadius(8)
+            .shadow(radius: 5)
+            .padding(.horizontal)
+            .padding(.top, 8)
+        }
     }
     
     var addCardButton: some View{
